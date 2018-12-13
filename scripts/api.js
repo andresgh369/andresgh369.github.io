a694b879a450982415697dbf2148722666d1fbce
Andres <44000958+andresgh369@users.noreply.github.com>
2018-12-13 19:03:31 -0300

    cambio url

--- scripts/api.js
+++ scripts/api.js
@@ -6,7 +6,7 @@
 
     var loadData = function() {
         var xhttp = new XMLHttpRequest();
-        var url = "http://andresgh369.pythonanywhere.com/producto/";
+        var url = "https://andresgh369.pythonanywhere.com/producto/";
 
         xhttp.onreadystatechange = function() {
             if( this.readyState == 4 && this.status == 200 ){
